22f12d595ae2538970027676af3c6ccb6ebfe00a
Lucas Tramonte <l182697@dac.unicamp.br>
2024-09-26 21:29:13 +0200

    code 1st project

--- Autonomous vehicle control laws/Init_Script.m
+++ Autonomous vehicle control laws/Init_Script.m
@@ -29,3 +29,37 @@ phieq = 20*pi/180;      % Equilibrum point for linearization
 
 %% Control
 Ltot = [sqrt(3) 1 0];
+
+%% LQR
+v = -vmax;
+b3 = v/b;
+a13 = -v*sin(phieq);
+a23 = v*cos(phieq);
+
+T_inv = [a23/(a13*b3) -1/b3 1/b3; 1/(a13*b3) 0 0; -a23/a13 1 0];
+
+A = [0 0 -v*sin(phieq); 0 0 v*cos(phieq); 0 0 0];
+B = [0; 0; v/b];
+
+Q = eye(2);
+Q(1,1) = 1;
+R = 10;
+A_til = T_inv*A*inv(T_inv);
+B_til = T_inv*B;
+
+[L,P,e] = lqr(A_til(1:2, 1:2),B_til(1:2),Q,R);
+
+Ltot = [L 0];
+
+
+
+%% Dynamique de l'actionneur direction
+s = tf('s');
+wn = 5;
+epsilon = 1;
+V_p = wn^2/(s^2+2*epsilon*wn*s+wn^2);
+V_pD = c2d(V_p, Te);
+
+FV_p = (s^2+2*epsilon*wn*s+wn^2)/(s^2+2*5*1*s+5^2);
+FV_pD = c2d(FV_p, Te);
+
